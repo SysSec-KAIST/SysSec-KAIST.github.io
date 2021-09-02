4f0c32698ef8a1918492de22eb13a7fbe002a572
Elliot Jung <2ngddo@gmail.com>
2021-09-02 22:25:41 +0900

    Initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+  },
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    quotes: ["error", "double", { avoidEscape: true }],
+    "react/prop-types": ["off"],
+    "react/jsx-props-no-spreading": "off",
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "no-nested-ternary": 0,
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
+    "no-console": "off",
+    "import/prefer-default-export": "off",
+    "no-confusing-arrow": "off",
+    "implicit-arrow-linebreak": "off",
+    semi: [2, "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "space-before-function-paren": ["error", "never"],
+    "vue/html-self-closing": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/html-quotes": "off",
+  },
+};
